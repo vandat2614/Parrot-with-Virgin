26695f64de0fbbbead9137b629e1d1ea5e8950b4
Triệu Tấn Đạt <dattantrieu20042@gmail.com>
2024-11-14 08:15:42 +0700

    Them nut hide khi an see more

--- app2.py
+++ app2.py
@@ -4,7 +4,8 @@ import os
 app = Flask(__name__)
 
 # Thư mục chứa hình ảnh
-IMAGE_FOLDER = os.path.join('static', 'images')
+CONTENT_IMAGE_FOLDER = os.path.join('static', 'images/content')
+STYLE_IMAGE_FOLDER = os.path.join('static', 'images/style')
 
 @app.route('/')
 def index():
@@ -15,13 +16,26 @@ def index():
 def main():
     return render_template('main.html')
 
-@app.route('/api/images')
-def get_images():
+@app.route('/api/content_images')
+def get_content_images():
     # Lấy danh sách hình ảnh từ thư mục
     try:
         images = [
-            f"{IMAGE_FOLDER}/{img}" 
-            for img in os.listdir(IMAGE_FOLDER) 
+            f"{CONTENT_IMAGE_FOLDER}/{img}" 
+            for img in os.listdir(CONTENT_IMAGE_FOLDER) 
+            if img.lower().endswith(('.png', '.jpg', '.jpeg', '.webp'))
+        ]
+        return jsonify(images)
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
+
+@app.route('/api/style_images')
+def get_style_images():
+    # Lấy danh sách hình ảnh từ thư mục
+    try:
+        images = [
+            f"{STYLE_IMAGE_FOLDER}/{img}" 
+            for img in os.listdir(STYLE_IMAGE_FOLDER) 
             if img.lower().endswith(('.png', '.jpg', '.jpeg', '.webp'))
         ]
         return jsonify(images)
