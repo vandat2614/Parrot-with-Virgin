4348d8346f0311ae34243e854de9ba9a705814d7
Van Datt <dovandat2614@gmail.com>
2024-12-07 22:48:05 +0700

    change \ to /

--- hyper.py
+++ hyper.py
@@ -6,13 +6,13 @@ STYLE_IMAGE_FOLDER = os.path.join(BASE_DIR, 'static', 'images', 'style')
 
 AdaAttN = 'AdaAttN'
 AdaAttN_encoder = 'TransferModel/AdaAttN/vgg_normalised.pth'
-AdaAttN_decoder = 'TransferModel\AdaAttN\latest_net_decoder.pth'
-AdaAttN_adattn_3 = 'TransferModel\AdaAttN\latest_net_adaattn_3.pth'
-AdaAttN_adattn_4_5 = 'TransferModel\AdaAttN\latest_net_transformer.pth'
+AdaAttN_decoder = 'TransferModel/AdaAttN/latest_net_decoder.pth'
+AdaAttN_adattn_3 = 'TransferModel/AdaAttN/latest_net_adaattn_3.pth'
+AdaAttN_adattn_4_5 = 'TransferModel/AdaAttN/latest_net_transformer.pth'
 
 AdaIN = 'AdaIN'
 AdaIN_encoder = AdaAttN_encoder
-AdaIN_decoder = 'TransferModel\AdaIN/adain_decoder.pth'
+AdaIN_decoder = 'TransferModel/AdaIN/adain_decoder.pth'
 
 TFStyleTransfer = 'TF-StyleTransfer'
 TF_model = 'https://tfhub.dev/google/magenta/arbitrary-image-stylization-v1-256/2'
